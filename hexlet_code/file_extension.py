de21d488337c9e1b730191e5d4edd8697d349c13
sergi <qasergievich@gmail.com>
2024-07-13 05:23:16 +0300

    add gendiff for recursive structures (step 6)

--- hexlet_code/file_extension.py
+++ hexlet_code/file_extension.py
@@ -10,11 +10,13 @@ EXT = {
 
 
 def get_file_extension(file_path):
+    """Получает расширение файла."""
     _, extension = os.path.splitext(file_path)
     return extension
 
 
 def deserialize(file_path):
+    """Десериализует файл в зависимости от его расширения."""
     extension = get_file_extension(file_path)
     for ext in EXT:
         if extension in ext:
